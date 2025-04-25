3a89ca785ffeb766f2c4f81ba445cee7b59ddef4
Marcos <juangura@gmail.com>
2025-04-25 10:18:50 -0400

    bump version

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: discourse-subscriptions
 # about: Allows admins to sell subscriptions to site content.
 # meta_topic_id: 140818
-# version: 2.8.2
+# version: 2.9.0
 # url: https://github.com/discourse/discourse-subscriptions
 # authors: Rimian Perkins, Justin DiRose
 
